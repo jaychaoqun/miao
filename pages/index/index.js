5f1841aecfab4b47f2c1536062c716e3de5f1af1
guozhongshan <18668314773@163.com>
2017-01-09 17:28:49 +0800

    阿斯达所多
    大叔大婶

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,62 @@
-//index.js
-//获取应用实例
-var app = getApp()
 Page({
-      data: {
-      }
+  data:{
+    lod:false,
+     markers: [{
+      iconPath: "/image1/diqu1.png",
+      latitude: 30.737290,
+      longitude: 120.819510,
+      width:30,
+      height:30,
+      scale:16
+    }]
+  }, 
+  onLoad:function(options){
+    // 生命周期函数--监听页面加载
+  },
+    getpos:function(){
+     wx.getLocation({
+    type: 'gcj02',
+    success: function(res) {
+    var latitude = res.latitude
+    var longitude = res.longitude
 
-  })
-
+          wx.openLocation({
+            latitude: latitude,
+            longitude: longitude,
+            scale: 28
+          })
+  }
+})
+  },
+  moveToLocation: function () {
+    this.mapCtx.moveToLocation()
+  },
+  onShow:function(){
+    // 生命周期函数--监听页面显示
+    String4
+  },
+  onHide:function(){
+    // 生命周期函数--监听页面隐藏
+    String5
+  },
+  onUnload:function(){
+    // 生命周期函数--监听页面卸载
+    String6
+  },
+  onPullDownRefresh: function() {
+    // 页面相关事件处理函数--监听用户下拉动作
+    String7
+  },
+  onReachBottom: function() {
+    // 页面上拉触底事件的处理函数
+    String8
+  },
+  onShareAppMessage: function() {
+    // 用户点击右上角分享
+    return {
+      title: 'title', // 分享标题
+      desc: 'desc', // 分享描述
+      path: 'path' // 分享路径
+    }
+  }
+})
