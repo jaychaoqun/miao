e52d2f5665b589a13637c48c2916259a0c914ee0
jaychaoqun <jaychaoaqun@126.com>
2017-01-11 16:42:28 +0800

    11
    11

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -2,7 +2,8 @@
 var util = require('../../utils/util.js')
 Page({
   data: {
-     tap:['保洁阿姨','保洁阿姨','保洁阿姨','保洁阿姨','保洁阿姨','保洁阿姨','保洁阿姨']
+     tap:['保洁阿姨','保洁阿姨','保洁阿姨','保洁阿姨','保洁阿姨','保洁阿姨','保洁阿姨'],
+     scroll_height:"500rpx"
   },
   onLoad: function () {
     this.setData({
@@ -10,5 +11,17 @@ Page({
         return util.formatTime(new Date(log))
       })
     })
+
+      try {
+      var res = wx.getSystemInfoSync()
+      var h = res.windowHeight-140;
+      this.setData({scroll_height:h+"px"});
+      
+    
+    } catch (e) {
+      // Do something when catch error
+    }
+
+
   }
 })
