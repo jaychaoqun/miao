d54e239207a25d28cafa3574c4a874b9be7a1198
valstr <270742966@qq.com>
2017-01-13 14:14:49 +0800

    页面细节修改

--- pages/logs/guanl/guanl.js
+++ pages/logs/guanl/guanl.js
@@ -21,17 +21,13 @@ Page({
             activeIndex: e.currentTarget.id
         });
     },
-  onLoad:function(options){
-    // 生命周期函数--监听页面加载
-    String2
-  },
   onReady:function(){
     // 生命周期函数--监听页面初次渲染完成
     String3
   },
   onShow:function(){
     // 生命周期函数--监听页面显示
-    String4
+    //String4
   },
   onHide:function(){
     // 生命周期函数--监听页面隐藏
